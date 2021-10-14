4f508f3cc47f38f698113ce10ed5928155c7f3a9
joao victor <joaovictorrrr93@gmail.com>
2021-10-13 22:59:26 -0300

    implementando botão

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, Button, TextInput, Alert} from 'react-native';
 
 export default function App() {
+  const [text, onChangeText] = React.useState("Digite aqui!");
   return (
     <View style={styles.container}>
       <Image source={require('./assets/react.png')} />
-      <StatusBar style="auto" />
+      <TextInput
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <Button
+      title="Pressione"
+      onPress={() => Alert.alert(text)}
+      />
     </View>
   );
 }
